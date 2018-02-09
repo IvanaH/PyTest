af912b676bac79ca5c95c61e8bb615dfd453c033
IvanaShM <ivana.shimin@gmail.com>
2018-02-09 18:42:01 +0800

    20180209

---
+++ Test/Hanoi.py
@@ -0,0 +1,19 @@
+#  Created by IvanaH on 02/02/18.
+#  coding: UTF-8
+
+def move(n):
+    # the origin one
+    lists1=[]
+    # the result
+    lists2=[]
+    lists3=[]
+    
+    for i in range(1,i):
+        lists1.append(i)
+    print lists1
+    
+    if n%2 == 0:
+        lists2[0] = lists1.pop(0)
+    else:
+        lists3[0] = lists1.pop(0)
+    
