4c6a9763075c94621c6a970c1dc246e79ea66f57
IvanaShM <ivana.shimin@gmail.com>
2018-02-13 18:32:20 +0800

    try hanoi with some help doc. 20180213

--- Test/Hanoi.py
+++ Test/Hanoi.py
@@ -1,46 +1,23 @@
 #  Created by IvanaH on 02/02/18.
 #  coding: UTF-8
+from test.test_threading_local import target
 
-def move(n, originSt, targetSt, dependSt):
-#     while(len(target)<n):
-        #n==1  1
-#         target.append(originSt.pop())
-        
-        #n==2 2 1
-#         cst.append(originSt.pop())
-#         target.append(originSt.pop())
-#         target.append(cst.pop())
-        
-#         #n ==3 2 1 1 2 1
-#         target.append(originSt.pop())
-#         cst.append(originSt.pop())
-#         cst.append(target.pop())
-#         target.append(originSt.pop())
-#         originSt.append(cst.pop())
-#         target.append(cst.pop())
-#         target.append(originSt.pop())
-#         
-#         #n==4  2 1 1 2 2 2 1 1 2 1
-#         cst.append(originSt.pop())
-#         target.append(originSt.pop())
-#         target.append(cst.pop())
-#         cst.append(originSt.pop())
-#         originSt.append(target.pop())
-#         cst.append(target.pop())
-#         cst.append(originSt.pop())
-#         target.append(originSt.pop())
-#         target.append(cst.pop())
-#         originSt.append(cst.pop())
-#         originSt.append(target.pop())
-#         target.append(cst.pop())
-#         cst.append(originSt.pop())
-#         target.append(originSt.pop())
-#         target.append(cst.pop())        
-        if n==1:
+def move(originSt, targetSt, dependSt):
+        if len(originSt)==1:
             target.append(originSt.pop())
         else:
             move(n-1)
             target.append(originSt.pop())
+        
+def hanoi(n):
+    stack originSt=[]
+    stack targetSt=[]
+    stack dependSt=[]
+    
+    for i in range(1,n):
+        originSt.append(i)
+
+    move(originSt, targetSt, dependSt)
         
         
     
