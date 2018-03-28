0a0ba2af3009153e939495b2054a3a6ac770fd2d
IvanaShM <ivana.shimin@gmail.com>
2018-03-28 21:29:07 +0800

    Try hanoi in class, and try ID_number_generator. 20180328

--- Test/HanoiII.py
+++ Test/HanoiII.py
@@ -1,54 +1,47 @@
 #  Created by IvanaH on 14/02/18.
 #  coding: UTF-8
 
-# class hanoi():
-    
-#     def __init__(self,n):
-#         self.n = n
-#         self.origin = []
-#         self.depend = []
-#         self.target = []
-#         for i in range(1,n):
-#             self.origin.append(i)
-#     origin = [1,2,3,4]
-#     depend = []
-#     target = []
-#             
-#     def move(n, origin, depend, target):
-#         print(origin)
-#         print(target)
-#         
-#         if n==1:
-# #         print (a,'-->',c)
-#            target.append(origin.pop())
-# #            print("From:"+self.origin)
-# #            print("To:"+self.target)
-#            return
-#         else:
-#            move(n-1,origin,target,depend)
-#            move(1,origin,depend,target)
-#            move(n-1,depend,origin,target)
-#         print(target)
+class hanoi():
+     
+    def __init__(self,n):
+        self.n = n
+        self.origin = []
+        self.depend = []
+        self.target = []
+        for i in range(1,n+1):
+            self.origin.append(i)
+             
+    def move(self, n, origin, depend, target):
+        print("The start is ",origin)
+         
+        if n==1:
+           target.append(origin.pop())
+           print(target)
+           return
+        else:
+           self.move(n-1,origin,target,depend)
+           self.move(1,origin,depend,target)
+           self.move(n-1,depend,origin,target)
+        print(target)
+         
+hanoiTest = hanoi(3)
+hanoiTest.move(hanoiTest.n,hanoiTest.origin,hanoiTest.depend,hanoiTest.target)
 
 
 # implement in method 
-origin = [1,2,3,4]
-depend = []
-target = []
-            
-def move(n, origin, depend, target):       
-    if n==1:
-        target.append(origin.pop())
-        print(target)
-        return
-    else:
-        move(n-1,origin,target,depend)
-        move(1,origin,depend,target)
-        move(n-1,depend,origin,target)
-        
-
-        
-# hanoiTest = hanoi(4)
-# hanoiTest.test(hanoiTest.origin)
-
-move(len(origin),origin,depend,target)
+# origin = [1,2,3,4]
+# depend = []
+# target = []
+#             
+# def move(n, origin, depend, target):       
+#     if n==1:
+#         target.append(origin.pop())
+#         print(target)
+#         return
+#     else:
+#         move(n-1,origin,target,depend)
+#         move(1,origin,depend,target)
+#         move(n-1,depend,origin,target)
+#     
+# 
+# move(len(origin),origin,depend,target)
---
+++ Test/ID_Number_Generator.py
@@ -0,0 +1,71 @@
+#  Created by IvanaH on 28/03/18.
+#  coding: UTF-8
+from datetime import date 
+from datetime import timedelta 
+from random import random, randint
+import re
+import codeop
+
+class generate():
+    def __init__(self):
+        self.idN = []
+    
+    def getDistrictCode(self):
+        with open('districtCode.txt') as file:
+            data = file.read()
+        dLists = data.splite('\n')
+        global codelist
+        codelist = []
+        pattern = re.compile('r\d+')
+        for dlist in dLists:
+            if pattern.match(dlist) :
+                codelist.append(pattern.match(dlist).group(0))
+        districtCode = codelist[randint(0,len(codelist))]
+        return districtCode
+        
+    
+    def getBirthDate(self):
+        year = randint(1920,2000)
+#         moth = randint(1,12)
+#         day = randint(1,31)
+        da = date.today()+timedelta(days=randint(1,366))
+        da  = da.strftime('%m%d')
+        return str(year)+da
+
+    
+    def getSerialNumber(self):
+        sn = randint(100,300)
+        return str(sn)
+    
+    def getCheckCode(self):
+        idN = self.getDistrictCode()+self.getBirthDate()+self.getSerialNumber()
+        
+        i = 0
+        count = 0
+        
+        weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+        SIN = {'0':'1','1':'0','2':'X','3':'9','4':'8','5':'7','6':'6','7':'5','8':'5','9':'3','10':'2'}
+        
+        for i in range(i,len(idN)):
+            count = count + int(weight[i])*int(idN[i])
+        count = count%11
+        
+        checkcode = SIN[str(count)]
+        
+        return checkcode
+        
+    
+    def getNumber(self):
+        return self.getDistrictCode()+self.getBirthDate()+self.getSerialNumber()+self.getCheckCode()    
+
+def test():
+    str = '123sdfkslkf sdkfdsklf'
+    pattern = re.compile(r'\d+')
+    if pattern.match(str):
+        return pattern.match(str).group(0)
+         
+    
+if __name__ == '__main__':
+#     print (test())
+    h = generate()
+    print(h.getNumber())
