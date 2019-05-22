e559e1849e75c79fc24e82f8131b4b67db14890d
Ivana_ShineMo <login.ivanna@gmail.com>
2019-05-22 15:16:44 +0800

    Relization of arithmetic by two stacks. 20190522

--- Py_DataStructure/stackL.py
+++ Py_DataStructure/stackL.py
@@ -15,21 +15,51 @@ def canculation (n1,n2,op):
         r = n1 - n2        
     elif op == '*':
         r = n1 * n2  
-    r = n1 / n2
-    return r
+    else:
+        r = n1 / n2
+    return int(r)
 
 def canculate(equation):
     stackNum = []
     stackOperator = []
     
     for i in range (0,len(equation)):
+        print(equation[i])
         if equation[i] == '*' or equation[i] == '/':
             stackOperator.append(equation[i])
-            stackNum.append(equation[i+1])
+            print("push into sO")
         elif equation[i] == '+' or equation[i] == '-':
-            if stackOperator[-1]== '*' or stackOperator[-1] == '/':
-                k = int(stackNum.pop())
+            if len(stackOperator) > 0:
+                flag = True
+                while flag:
+                    if stackOperator[-1]== '*' or stackOperator[-1] == '/':
+                        k = int(stackNum.pop())
+                        j = int(stackNum.pop())
+                        op = stackOperator.pop()
+                        r = canculation(j, k, op)
+                        stackNum.append(r)
+                        print("change num %i,%i,%s,%i",j,k,op,r)
+                        continue
+                    flag = False
             stackOperator.append(equation[i])
-            stackNum.append(equation[i+1])
+            print("push into sO")
+        else:
+            stackNum.append(equation[i])
+            print("push into sN")
+    
+    print(stackNum)
+    print(stackOperator)
+
+    for i in range(0,len(stackOperator)):
+        k = int(stackNum.pop())
+        j = int(stackNum.pop())
+        op = stackOperator.pop()
+        stackNum.append(canculation(j, k, op))
+    print (stackNum.pop())
+    
+if __name__ == '__main__':
+    eq = '5+6*4/2-8'
+    canculate(eq)
+
         
             
