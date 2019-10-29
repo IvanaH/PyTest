d5f56bc641384d2883f1c44ffe43ff06fbabb2e9
Ivana_ShineMo <login.ivanna@gmail.com>
2019-10-29 12:07:31 +0800

    learning of params in function, especially about default parameter, which can be assigned by keyword and position. 2019.10.29

--- Test/paramsL.py
+++ Test/paramsL.py
@@ -13,8 +13,8 @@ def variadicP(p1 = None,*args):
     else:
         print("p1 is by %s, and others is %s" % (p1,args))   
 
-def defaultAndVariadicArgu(a,b=1,*args):  
-    print("a = %s,b= %s, and the variadic arguments are: %s" %(a,b,args))  
+def defaultAndVariadicArgu(a,b=10,c=2,*args):  
+    print("a = %s,b= %s,c=%s, and the variadic arguments are: %s" %(a,b,c,args))  
     
 def keywordP(p1,*,kw1,kw2,**kw):
     print("Function name:%s, and param p1 is %s" % (self.__name__,p1))
@@ -25,11 +25,14 @@ def mixedP(p1,*args,kw1,kw2):
     print("Function name:%s, and param p1 is %s" % (self.__name__,p1))
     print("%s are variadic paramters which accpeted as tuple, %s:%s and %s:%s are named keyword paramters." %(args, kw1.__name__,kw1,kw2.__name__,kw2))
     
+def f1(a,b,c=0,*args,**kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
 if __name__ == '__main__':
-    p1V = 'fixed paramter'
-    kw1V, kw2V = 1,2
-    dict0 = {'d1':11,'d2':22}
-    list0 = list("this is a list")
+#     p1V = 'fixed paramter'
+#     kw1V, kw2V = 1,2
+#     dict0 = {'d1':11,'d2':22}
+#     list0 = list("this is a list")
     
 #     variadicP(p1 = p1V)
 #     variadicP('args1','args2')
@@ -42,9 +45,15 @@ if __name__ == '__main__':
 #     keywordP(p1V,kw1=kw1V,kw2=kw2V,d3=33,d4=44)
 #      
 #     mixedP(p1=p1V,*list0,kw1=kw1V,kw2=kw2V)
+#     
+#     defaultAndVariadicArgu(0, 3)
+#     defaultAndVariadicArgu(0, 5,args=(1,2))   
+#     defaultAndVariadicArgu(0, c=5)   
+
+    f1(1, 2, 3, 'a', 'b', x=99)  # a = 1 b = 2 c = 3 args = ('a', 'b') kw = {'x': 99}
+    f1(1,2,x=99,y=98,c=97)    # a = 1 b = 2 c = 97 args = () kw = {'x': 99, 'y': 98}
+    f1(1,2,10,x=99,y=98,c=97)   # TypeError: f1() got multiple values for argument 'c'
     
-#     defaultAndPositionalArgu(0, 3)
-    defaultAndPositionalArgu(0, b=3, 4, 5)    
-    
+
     
     
